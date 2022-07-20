5face2a5379da5637b0cc0074f1c8fc1c6443189
Sebastian Meckovski <Sebastian.Meckovski@streets-heaver.com>
2022-07-20 09:32:04 +0100

    axios installed and implemented

--- src/pages/tasks2/tasks2.js
+++ src/pages/tasks2/tasks2.js
@@ -3,6 +3,7 @@ import DataGrid, {Column, FilterRow, HeaderFilter, Paging} from "devextreme-reac
 import { Button } from 'devextreme-react/button';
 import { dataSource4 } from "../tasks2/testData2";
 import { useScreenSizeClass } from "./media-query";
+import axios from "axios";
 
 export default function RemindersGrid({
     selectPatient,
@@ -17,20 +18,39 @@ export default function RemindersGrid({
     const smallScreen =
     screenSize === "screen-small" || screenSize === "screen-x-small";
 
+
+    // Old Way of writing: 
+
+    // function handleClick(){
+    //     const req = new XMLHttpRequest();
+    //     const url = "https://localhost:5001/api/samurais";
+
+    //     req.open('GET', url);
+    //     req.send();
+    //     req.onreadystatechange = () => 
+    //     {
+    //         let responseText = req.responseText;
+    //         var data = JSON.parse(responseText);
+    //         setSamurais(data)
+    //     }
+    // }
+
+    // New Way of writing:
+
     function handleClick(){
-        const req = new XMLHttpRequest();
         const url = "https://localhost:5001/api/samurais";
 
-        req.open('GET', url);
-        req.send();
-        req.onreadystatechange = () => 
-        {
-            let responseText = req.responseText;
-            var data = JSON.parse(responseText);
-            setSamurais(data)
-        }
+        axios.get(url)
+            .then(res => 
+                {
+                    const responseData = res.data;
+                    setSamurais(responseData)
+                });
+
+        
     }
 
+    const axios = require('axios').default;
 
     return (
 
